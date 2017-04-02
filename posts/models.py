7a650da6907368a3e6e092c8fa553b9d79f8c44c
n_coder <n@example.com>
2017-04-02 16:23:50 +0530

    Included Search, Permissions for Admin/staff and Users & some other changes.

--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.core.urlresolvers import reverse
 from django.conf import settings
-
+from django.utils import timezone
 
 #to specify image upload location we define a function
 
 #def upload_location(instance, filename):
    # return "%s/%s" % (instance.pk, filename)
+
+class PostManager(models.Manager):
+    def active(self, *args, **kwargs):
+        return super(PostManager,self).filter(draft=False).filter(publish__lte=timezone.now())
 
 
 class Post(models.Model):
@@ -17,9 +21,13 @@ class Post(models.Model):
                               #upload_to=upload_location)
 
     content = models.TextField()
+    draft = models.BooleanField(default=False)
+    publish = models.DateField(auto_now=False, auto_now_add=False)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
+
+    objects = PostManager()
     def __str__(self):
         return self.title
 
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
 from urllib.parse import quote_plus
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.utils import timezone
+from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 # from django.core.urlresolvers import reverse
@@ -32,6 +34,9 @@ def post_create(request):
 
 def post_detail(request, pk=None):
     instance = get_object_or_404(Post, pk=pk)
+    if instance.publish > timezone.now().date() or instance.draft:
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
     share_string = quote_plus(instance.content)
     context = {
         'title': instance.title,
@@ -42,10 +47,23 @@ def post_detail(request, pk=None):
 
 
 def post_list(request):
-    queryset_list = Post.objects.all()
+    today = timezone.now().date()
+    queryset_list = Post.objects.active()
+    if not request.user.is_staff or not request.user.is_superuser:
+        queryset_list = Post.objects.all()
+
+    query = request.GET.get('q')
+    if query:
+        queryset_list = queryset_list.filter(
+                        Q(title__icontains=query)|
+                        Q(content__icontains =query)|
+                        Q(user__first_name__contains=query)|
+                        Q(user__last_name__contains=query)
+                        )
     paginator = Paginator(queryset_list, 10)  # Show 25 contacts per page
 
-    page = request.GET.get('page')
+    page_request_var = "page"
+    page = request.GET.get(page_request_var)
     try:
         queryset = paginator.page(page)
     except PageNotAnInteger:
@@ -57,7 +75,9 @@ def post_list(request):
 
     context = {
         'objects_list': queryset,
-        'title': 'List'
+        'title': 'List',
+        'page_request_var': page_request_var,
+        'today': today,
     }
 
     return render(request, 'posts_list.html', context)
