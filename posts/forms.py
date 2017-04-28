04c832974d2bbcb4c2a23cec59736242c4b58f7a
codenit <b.rohit751@gmail.com>
2017-04-28 17:44:34 +0530

    Dynamic update of Content and Title

--- posts/forms.py
+++ posts/forms.py
@@ -3,7 +3,7 @@ from pagedown.widgets import PagedownWidget
 from .models import Post
 
 class PostForm(forms.ModelForm):
-    content = forms.CharField(widget=PagedownWidget)
+    content = forms.CharField(widget=PagedownWidget(show_preview=False))
     publish = forms.DateField(widget=forms.SelectDateWidget)
     class Meta:
         model = Post
